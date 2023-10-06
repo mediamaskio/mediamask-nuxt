c52131dc09945b6ebf04a7ae3b2f844bfb9c75f2
Gregor Vostrak <gregorvostrak@Gregors-MacBook-Pro.local>
2023-10-06 17:51:10 +0200

    add typescript config

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,4 +1,13 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: 'build',
+    shim: false,
+    tsConfig: {
+      compilerOptions: {
+        moduleResolution: 'bundler',
+      },
+    },
+  }
 })
